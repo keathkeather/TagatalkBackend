11809a07a9ea101f31ac16cb29b2f1bc83e522f8
keathkeather <67945664+keathkeather@users.noreply.github.com>
2024-04-07 00:30:39 +0800

    Delete prisma directory

--- prisma/migrations/20240401161647_user_rls/migration.sql
+++
@@ -1,9 +0,0 @@
--- This is an empty migration.
-
-ALTER TABLE "User" ENABLE ROW LEVEL SECURITY;
-ALTER TABLE "User" FORCE ROW LEVEL SECURITY;
-
--- Add a policy that allows users to only select their own records
-CREATE POLICY user_self_policy ON "User" 
-  FOR SELECT 
-  USING ("id" = current_setting('app.current_user_id', TRUE));
