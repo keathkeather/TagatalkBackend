d584e0b4c8333ec6398d7662d715471687f25ef2
keathkeather <keath.ian@gmail.com>
2024-04-28 12:01:15 +0800

    [KET][UPDATE]Update on port

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
 
-
-// const httpsOptions = {
-//   key: fs.readFileSync('./src/cert/key.pem'),
-//   cert: fs.readFileSync('./src/cert/cert.pem'),
-// };
+const port = process.env.PORT || 3000;
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
-  await app.listen(3000);
+  await app.listen(port);
+  console.log(`Server is running on port ${port}`);
 }
 bootstrap();
