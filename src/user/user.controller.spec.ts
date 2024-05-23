a1d3235628a011274b04c97a9e138a54564a83bb
keathkeather <keath.ian@gmail.com>
2024-05-23 18:00:48 +0800

    [KET][ADD] update on game

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -5,12 +5,13 @@ import { PrismaService } from '../prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { AuthService } from '../auth/auth.service';
 import { MailerService } from '../mailer/mailer.service';
+import { GameService } from '../game/game.service';
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],providers:[UserService,PrismaService,JwtService,AuthService,MailerService]
+      controllers: [UserController],providers:[UserService,PrismaService,JwtService,AuthService,MailerService,GameService]
     }).compile();
 
     controller = module.get<UserController>(UserController);
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -4,12 +4,13 @@ import { PrismaService } from '../prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { AuthService } from '../auth/auth.service';
 import { MailerService } from '../mailer/mailer.service';
+import { GameService } from '../game/game.service';
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService,PrismaService,JwtService,AuthService,MailerService],
+      providers: [UserService,PrismaService,JwtService,AuthService,MailerService,GameService],
     }).compile();
 
     service = module.get<UserService>(UserService);
