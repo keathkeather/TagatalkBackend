6518b3a532a5699be3febd126d1cec4baaef761d
keathkeather <keath.ian@gmail.com>
2024-10-21 21:54:33 +0800

    [KET][UPDATE] update on mailer service endpoint

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -13,7 +13,7 @@ export class MailerService {
         });
       }
       async sendVerificationEmail(email: string, token: string) {
-        const url = `http://${process.env.VERIFICATION_IP}:3000/v1/auth/verify/${token}`;
+        const url = `https://${process.env.VERIFICATION_URL}/v1/auth/verify/${token}`;
     
         await this.transporter.sendMail({
           to: email,
